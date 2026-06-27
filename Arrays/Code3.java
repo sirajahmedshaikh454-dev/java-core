bf8500ad734e0565943a805a1ed9485ad11c9443
Siraj Ahmed <sirajahmedshaikh454@gmail.com>
2026-06-27 16:04:47 +0500

    Strings

---
+++ Arrays/Code3.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+public class Code3 {
+    public static void main(String[] args) {
+        // Strings
+        String firstName = "Aditya";
+        String lastName = "Tandon";
+
+        String fullName = firstName + " " + lastName;
+
+        System.out.println(fullName); // Aditya Tandon
+    }
+}
